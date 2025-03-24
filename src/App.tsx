b9506fe18b91bd1e19b958736a8548d5ef9f5631
Anh Ngoc <ngocda@ows.com.vn>
2025-03-24 19:50:14 +0700

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,15 @@
-import React, { useEffect } from "react";
 import "./App.css";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
-import {
-  Box,
-  createTheme,
-  LoadingOverlay,
-  MantineProvider,
-} from "@mantine/core";
-import {
-  BrowserRouter,
-  Navigate,
-  Outlet,
-  Route,
-  Routes,
-  useNavigate,
-} from "react-router-dom";
+import { createTheme, LoadingOverlay, MantineProvider } from "@mantine/core";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./screens/home";
 import Login from "./screens/login";
 import Lazada from "./screens/lazada";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./services/query-client";
 import { Provider, observer } from "mobx-react";
 import { uiStore } from "./stores/ui";
-import { Notification } from "@mantine/core";
-import { IconX } from "@tabler/icons-react";
 import { ToastContainer } from "react-toastify";
 const App = observer(() => {
   const theme = createTheme({});
